a0a360de326153abbe77c697ef1b641077393b48
nadunbandara20070426-ux <nadunbandara20070426@gmail.com>
2025-12-16 01:51:58 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config.jsmodule.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
